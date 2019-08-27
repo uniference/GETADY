429dd7fb87a62c1f4ccdcc1f937b556d22d22a39
SRL-09 <52871888+SRL-09@users.noreply.github.com>
2019-08-26 22:59:59 -0500

    creando componentes

--- GETADY/src/app/app.module.ts
+++ GETADY/src/app/app.module.ts
@@ -3,10 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './componentes/navbar/navbar.component';
+import { Page404Component } from './componentes/page404/page404.component';
+import { HomeComponent } from './componentes/home/home.component';
+import { LoginComponent } from './componentes/user/login/login.component';
+import { RegisterComponent } from './componentes/user/register/register.component';
+import { CreateAdComponent } from './componentes/create-ad/create-ad.component';
+import { PeriodComponent } from './componentes/period/period.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    Page404Component,
+    HomeComponent,
+    LoginComponent,
+    RegisterComponent,
+    CreateAdComponent,
+    PeriodComponent
   ],
   imports: [
     BrowserModule,
